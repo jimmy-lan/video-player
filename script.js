97e0df058f198d2c6205cb3b9fe04dd42c728f22
Jimmy <jimmylanbcn@gmail.com>
2020-08-07 19:29:39 -0400

    Add selectors

---
+++ script.js
@@ -0,0 +1,11 @@
+// Selectors
+const videoPlayer = document.querySelector("video.video");
+const progressRange = document.querySelector(".progress-range");
+const progressBar = document.querySelector(".progress-bar");
+const btnPlay = document.getElementById("btn-play");
+const btnVolume = document.getElementById("btn-volume");
+const volumeRange = document.querySelector(".volume-range");
+const volumeBar = document.querySelector(".volume-bar");
+const lblCurrentTime = document.querySelector(".time-elapsed");
+const lblDuration = document.querySelector(".time-duration");
+const btnFullscreen = document.getElementById("btn-fullscreen");
